a725f59b3973df48d527cf60d92155138e3a7736
VictorRameh <vicrameh@gmail.com>
2022-09-03 14:00:39 -0300

    atualização

--- PersonalCarros/receitas/views.py
+++ PersonalCarros/receitas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
+
+def ferrari(request):
+    return render(request,"ferrari.html")
